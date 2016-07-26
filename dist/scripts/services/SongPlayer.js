06b03b1c880e79be1afe17b857fe21c1f78635a3
David Roberts <drob@drob.net>
2016-07-26 00:10:03 -0400

    AS 7 - playSong function

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,9 @@
 (function() {
    function SongPlayer() {
+      /**
+      * @desc Song Player external interface
+      * @type {Object}
+      */
       var SongPlayer = {};
       var currentSong = null;
 
@@ -28,11 +32,25 @@
         currentSong = song;
       };
 
+      /**
+      * @function playSong
+      * @desc Plays the current song
+      * @param {Object} song
+      */
+      var playSong = function(song) {
+        currentBuzzObject.play()
+        song.playing = true;
+      }
+
+      /**
+      * @function play
+      * @desc publically accessible interfaction to play a song
+      * @param {Object} the song to play
+      */
       SongPlayer.play = function(song) {
         if (currentSong !== song) {
           setSong(song);
-          currentBuzzObject.play();
-          song.playing = true;
+          playSong(song);
         } else if (currentSong === song) {
           if (currentBuzzObject.isPaused()) {
              currentBuzzObject.play();
@@ -40,6 +58,11 @@
         }
      };
 
+     /**
+     * @function pause
+     * @desc publically accessible interfaction to pause a song
+     * @param {Object} the song to pause
+     */
      SongPlayer.pause = function(song) {
        currentBuzzObject.pause();
        song.playing = false;
